db12d17f75b8e7ad0d84b31a6d2b73e74355dc22
dudung <dudung@gmail.com>
2022-05-19 19:32:01 +0700

    update +2

--- butiran/vect3.py
+++ butiran/vect3.py
@@ -1,6 +1,7 @@
 # 20220519
 # 1909 __init__, _str__ and tested ok.
 # 1915 __add__, __sub__ and tested ok.
+# 1928 __mul__, __rmul__ and tested ok.
 
 class Vect3:
   def __init__(self, x=0, y=0, z=0):
@@ -28,4 +29,22 @@ class Vect3:
     r.x = self.x - other.x
     r.y = self.y - other.y
     r.z = self.z - other.z
+    return r
+
+  def __mul__(self, other):
+    r = Vect3()
+    if isinstance(other, int) | isinstance(other, float):
+      r.x = self.x * other
+      r.y = self.y * other
+      r.z = self.z * other
+    elif isinstance(other, Vect3):
+      r.x = self.y * other.z - self.z * other.y
+      r.y = self.z * other.x - self.x * other.z
+      r.z = self.x * other.y - self.y * other.x
+    return r
+
+  def __rmul__(self, other):
+    r = Vect3()
+    if isinstance(other, int) | isinstance(other, float):
+      r = self.__mul__(other)
     return r
--- test/test_vect3.py
+++ test/test_vect3.py
@@ -1,7 +1,10 @@
+# to try a unit test from root folder use
 # python -m test.test_vect3
 
 # import Vect3 class
 from butiran.vect3 import Vect3
+
+# set False to True to show the results of a test block
 
 # initialize an instance -- 20220519
 if False:
@@ -29,16 +32,27 @@ if False:
   r = Vect3(y=2, x=1)
   print(r)
 
-# overload addition operator
+# overload addition operator -- 20220519
 if False:
   r1 = Vect3(1, 2, 3)
   r2 = Vect3(-1, -2, -3)
   r3 = r1 + r2
   print(r1, "+", r2, "=", r3)
 
-# overload addition operator
+# overload addition operator -- 20220519
 if False:
   r1 = Vect3(1, -2, 3)
   r2 = Vect3(-1, 2, -3)
   r3 = r1 - r2
   print(r1, "-", r2, "=", r3)
+
+# overload multiplication operator -- 20220519
+if False:
+  r = Vect3(1, -2, 3)
+  c = 2
+  s = r * c
+  print(r, "*", c, "=", s)
+  t = c * r
+  print(c, "*", r, "=", t)
+  u = s * t
+  print(s, "*", t, "=", u)
