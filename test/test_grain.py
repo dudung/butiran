cc15e73a848404a2b1ec1315dbbfb2386167746b
dudung <dudung@gmail.com>
2022-09-14 17:49:03 +0700

    update, new module, grain

---
+++ test/test_grain.py
@@ -0,0 +1,17 @@
+# test_grain.py
+# unit test of grain module
+# Sparisoma Viridi | https://github.com/dudung
+
+# add folder butiran to path -- 20220914
+import sys
+sys.path.insert(0, '../../butiran')
+
+# import Grain class
+from butiran.grain import Grain
+
+# set False to True to show the results of a test block
+
+# initialize an instance -- 20220914
+if True:
+  g = Grain()
+  print(g)
