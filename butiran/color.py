a551ceeb14006a8fe9702f5a65c4a5fae81970b0
dudung <dudung@gmail.com>
2022-09-14 20:58:12 +0700

    new, update

---
+++ butiran/color.py
@@ -0,0 +1,22 @@
+# color.py
+# color module
+# Sparisoma Viridi | https://github.com/dudung
+
+# 20220914
+#   2039 Learn to make this module.
+
+# add folder butiran to path -- 20220914
+import sys
+sys.path.insert(0, '../../butiran')
+
+class Color:
+  def __init__(self, stroke='#000', fill='#fff'):
+    self.stroke = stroke
+    self.fill = fill
+  
+  def __str__(self):
+    str = '{ '
+    str += f'"stroke": {self.stroke}' + ', '
+    str += f'"fill": {self.fill}'
+    str += ' }'
+    return str
---
+++ tests/test_color.py
@@ -0,0 +1,17 @@
+# test_color.py
+# unit test of color module
+# Sparisoma Viridi | https://github.com/dudung
+
+# add folder butiran to path -- 20220914
+import sys
+sys.path.insert(0, '../../butiran')
+
+# import Grain class
+from butiran.color import Color
+
+# set False to True to show the results of a test block
+
+# initialize an instance -- 20220914
+if True:
+  c = Color()
+  print(c)
