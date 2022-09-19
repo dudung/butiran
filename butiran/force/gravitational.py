20bdb56b29b8f192628f24f8a965acb587db50da
dudung <dudung@gmail.com>
2022-09-19 17:45:10 +0700

    create gravitational module

---
+++ butiran/force/gravitational.py
@@ -0,0 +1,28 @@
+# gravitational.py
+# module for gravitational force of a point mass particle
+# Sparisoma Viridi | https://github.com/dudung
+
+# 20220919
+#   1731 Start this module.
+#   1743 Finish test it.
+
+from butiran.math.vect3 import Vect3
+from butiran.grain import Grain
+
+class Gravitational:
+  def __init__(self, field=Vect3()):
+    assert isinstance(field, Vect3), 'field should be a Vect3'
+    self.field = field
+  
+  def __str__(self):
+    str = '{\n'
+    str += f'  "field": "{self.field}"' + ',\n'
+    str += '}'
+    return str
+  
+  def force(self, grain):
+    assert isinstance(grain, Grain)
+    m = grain.m
+    g = self.field
+    f = m * g
+    return f
