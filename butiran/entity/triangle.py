c98c65d5f3de19320572c6a8df1518bd8ed68955
dudung <dudung@gmail.com>
2023-05-24 17:38:39 +0700

    rename variables

--- butiran/entity/triangle.py
+++ butiran/entity/triangle.py
@@ -4,6 +4,7 @@
 
 # 20230524
 #   0508 Start this module.
+#   1737 Rename r0, r1, r2 to p0, p1, p2.
 
 from butiran.math.vect3 import Vect3
 from butiran.entity.color2 import Color2
@@ -12,14 +13,14 @@ class Triangle:
   def __init__(
     self, id="0000", m=0, q=0, b=0,
     color=Color2(),
-    r0=Vect3(0, 0, 0),
-    r1=Vect3(1, 0, 0),
-    r2=Vect3(0, 1, 0)
+    p0=Vect3(0, 0, 0),
+    p1=Vect3(1, 0, 0),
+    p2=Vect3(0, 1, 0)
   ):
     self.id = id
-    self.r0 = r0
-    self.r1 = r1
-    self.r2 = r2
+    self.p0 = p0
+    self.p1 = p1
+    self.p2 = p2
     self.m = m
     self.q = q
     self.b = b
@@ -28,9 +29,9 @@ class Triangle:
   def __str__(self):
     str = '{\n'
     str += f'  "id": "{self.id}"' + ',\n'
-    str += f'  "r0": {self.r0}' + ',\n'
-    str += f'  "r1": {self.r1}' + ',\n'
-    str += f'  "r2": {self.r2}' + ',\n'
+    str += f'  "p0": {self.p0}' + ',\n'
+    str += f'  "p1": {self.p1}' + ',\n'
+    str += f'  "p2": {self.p2}' + ',\n'
     str += f'  "m": {self.m}' + ',\n'
     str += f'  "q": {self.q}' + ',\n'
     str += f'  "b": {self.b}' + ',\n'
--- tests/instantiation/triangle.py
+++ tests/instantiation/triangle.py
@@ -4,6 +4,7 @@
 
 # 20230524
 #   0514 Start test of triangle module.
+#   1738 Rename r0, r1, r2, to p0, p1, p2
 
 from butiran.math.vect3 import Vect3
 from butiran.entity.color2 import Color2
@@ -12,9 +13,9 @@ from butiran.entity.triangle import Triangle
 if True:
   tie = Triangle(
     m=1.23,
-    r0=Vect3(0, 0, 2),
-    r1=Vect3(1, 0, 2),
-    r2=Vect3(0, 1, 2),
+    p0=Vect3(0, 0, 2),
+    p1=Vect3(1, 0, 2),
+    p2=Vect3(0, 1, 2),
     color=Color2('#0f0', '#ff0')
   )
   
@@ -24,9 +25,9 @@ if True:
 $ python triangle.py
 {
   "id": "0000",
-  "r0": { "x": 0, "y": 0, "z": 2 },
-  "r1": { "x": 1, "y": 0, "z": 2 },
-  "r2": { "x": 0, "y": 1, "z": 2 },
+  "p0": { "x": 0, "y": 0, "z": 2 },
+  "p1": { "x": 1, "y": 0, "z": 2 },
+  "p2": { "x": 0, "y": 1, "z": 2 },
   "m": 1.23,
   "q": 0,
   "b": 0,
